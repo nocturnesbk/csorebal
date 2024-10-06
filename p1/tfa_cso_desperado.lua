bdbe64f17f19a80317a60006db4b49bd9e2defd8
ren <149859161+stxrgzr@users.noreply.github.com>
2024-10-05 21:05:49 -0500

    the spread was really really high so i turned it down

--- p1/tfa_cso_desperado.lua
+++ p1/tfa_cso_desperado.lua
@@ -63,7 +63,7 @@ SWEP.Primary.KickDown = 0.00 -- This is the maximum downwards recoil (skeet)
 SWEP.Primary.KickHorizontal = 0.0 -- This is the maximum sideways recoil (no real term)
 SWEP.Primary.StaticRecoilFactor = 0.5 --Amount of recoil to directly apply to EyeAngles.  Enter what fraction or percentage (in decimal form) you want.  This is also affected by a convar that defaults to 0.5.
 --Firing Cone Related
-SWEP.Primary.Spread = .1 --This is hip-fire acuracy.  Less is more (1 is horribly awful, .0001 is close to perfect)
+SWEP.Primary.Spread = .02 --This is hip-fire acuracy.  Less is more (1 is horribly awful, .0001 is close to perfect)
 SWEP.Primary.IronAccuracy = .01 -- Ironsight accuracy, should be the same for shotguns
 --Unless you can do this manually, autodetect it.  If you decide to manually do these, uncomment this block and remove this line.
 SWEP.Primary.SpreadMultiplierMax = 1.5 --How far the spread can expand when you shoot.
